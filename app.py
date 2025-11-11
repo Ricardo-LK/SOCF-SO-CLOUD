41565a47cbed4ddc9505a0e445f3fa4c042a1372
Ricardo L.K. <92482482+Ricardo-LK@users.noreply.github.com>
2025-11-11 11:35:24 -0300

    Fix HTML formatting in index and metricas routes

--- app.py
+++ app.py
@@ -27,11 +27,11 @@ print(json.dumps(metricas, ensure_ascii=False))
 
 @app.route("/")
 def index():
-    return f"Nome: <h1>Ricardo Lucas Kucek</h1> \
-    <br>So: <p>{platform.platform()}</p> \
-    <br>PID: <p>{os.getpid()}</p> \
-    <br>Porcentagem CPU: <p>{psutil.cpu_percent()}</p> \
-    <br>Uso de Memoria(Bytes): <p>{psutil.virtual_memory().used // (1024 ** 2)}</p>"
+    return f"<h1>Nome: Ricardo Lucas Kucek</h1> \
+    <br><p>So: {platform.platform()}</p> \
+    <br><p>PID: {os.getpid()}</p> \
+    <br><p>Porcentagem CPU: {psutil.cpu_percent()}</p> \
+    <br><p>Uso de Memoria(Bytes): {psutil.virtual_memory().used // (1024 ** 2)}</p>"
 
 
 @app.route("/info")
@@ -41,4 +41,4 @@ def info():
 
 @app.route("/metricas")
 def metricas():
-    return f"<h1>{metricas}</h1>"
+    return f"<h1>{json.dumps(metricas, ensure_ascii=False)}</h1>"
